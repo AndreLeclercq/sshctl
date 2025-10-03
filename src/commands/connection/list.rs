c5209b6794ea7a5c0aec9b7e673cd2079a29b1d9
Andre Leclercq <andre.leclercq@oclock.io>
2025-10-03 14:25:14 +0200

    🎨 Modularize all connection commands

---
+++ src/commands/connection/list.rs
@@ -0,0 +1,16 @@
+use anyhow::{Context, Result};
+use crate::config::{
+    get_connection_file_content, 
+    Config,
+};
+
+pub fn list() -> Result<()> {
+    let config_file: Config = get_connection_file_content().context("Error when getting connection file content.")?;
+    if let Some(connections) = config_file.connection {
+        eprintln!("List of connections:");
+        for name in connections.keys() {
+            eprintln!("{}", name);
+        }
+    }
+    Ok(())
+}
